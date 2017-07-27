90234769c684abf5312d7f227d730c81f8fdb9f1
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-27 01:07:19 -0400

    IHasModel 4 block

--- src/main/java/shadows/how2mod/blocks/BlockTest.java
+++ src/main/java/shadows/how2mod/blocks/BlockTest.java
@@ -2,12 +2,16 @@ package shadows.how2mod.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.model.ModelLoader;
 import shadows.how2mod.How2Mod;
+import shadows.how2mod.client.IHasModel;
 import shadows.how2mod.init.ModRegistry;
 
-public class BlockTest extends Block {
+public class BlockTest extends Block implements IHasModel {
 
 	public BlockTest(String name) {
 		super(Material.ROCK);
@@ -16,6 +20,11 @@ public class BlockTest extends Block {
 		setCreativeTab(CreativeTabs.MISC);
 		ModRegistry.BLOCKS.add(this);
 		ModRegistry.ITEMS.add(new ItemBlock(this).setRegistryName(getRegistryName()));
+	}
+
+	@Override
+	public void registerModels() {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
 	}
 
 }
