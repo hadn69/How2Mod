48273e7f55bf3c71cb001c42336fba541ff72ff9
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-27 04:28:57 -0400

    functional test item

--- src/main/java/shadows/how2mod/items/ItemTest.java
+++ src/main/java/shadows/how2mod/items/ItemTest.java
@@ -2,7 +2,16 @@ package shadows.how2mod.items;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import shadows.how2mod.How2Mod;
 import shadows.how2mod.client.IHasModel;
@@ -21,5 +30,19 @@ public class ItemTest extends Item implements IHasModel{
 	public void registerModels() {
 		ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
 	}
+	
+	@Override
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        if(world.isRemote)
+        	player.sendMessage(new TextComponentString("You have right clicked with a " + this.getRegistryName()));
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+    }
+	
+		@Override
+	    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+	        if(world.isRemote)
+	        	player.sendMessage(new TextComponentString("You have right clicked a " + world.getBlockState(pos).getBlock().getRegistryName() + " with a " + this.getRegistryName()));
+	        return EnumActionResult.SUCCESS;
+	    }
 
 }
