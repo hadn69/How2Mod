860899525689181cf2b8ac2d3de3c17da3757b65
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-26 22:05:20 -0400

    Mod annotation

--- src/main/java/shadows/how2mod/How2Mod.java
+++ src/main/java/shadows/how2mod/How2Mod.java
@@ -1,5 +1,12 @@
 package shadows.how2mod;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = How2Mod.MODID, name = How2Mod.NAME, version = How2Mod.VERSION)
 public class How2Mod {
+	
+	public static final String MODID = "how2mod";
+	public static final String NAME = "How 2 Mod";
+	public static final String VERSION = "1.0.0";
 
 }
