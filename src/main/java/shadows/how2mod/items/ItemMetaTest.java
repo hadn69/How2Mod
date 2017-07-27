458ab28c8e21dfb4333bd6e65e42a2f60f7feba6
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-27 03:34:10 -0400

    Meta item

---
+++ src/main/java/shadows/how2mod/items/ItemMetaTest.java
@@ -0,0 +1,31 @@
+package shadows.how2mod.items;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.client.model.ModelLoader;
+
+public class ItemMetaTest extends ItemTest{
+
+	public ItemMetaTest(String name) {
+		super(name);
+		setHasSubtypes(true);
+	}
+	
+	@Override
+	public int getMetadata(int damage) {
+		return damage;
+	}
+	
+	@Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> list) {
+        if (this.isInCreativeTab(tab)) for(int i = 0; i < 16; i++) list.add(new ItemStack(this, 1, i));
+    }
+	
+	@Override
+	public void registerModels() {
+		for(int i = 0; i < 16; i++) ModelLoader.setCustomModelResourceLocation(this, i, new ModelResourceLocation(getRegistryName(), "meta=" + i));
+	}
+
+}
