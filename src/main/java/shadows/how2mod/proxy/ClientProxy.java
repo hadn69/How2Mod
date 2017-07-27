52c83bf11b620acf372bdcde06104857fd5c34df
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-26 22:45:51 -0400

    Last bits of framewor

--- src/main/java/shadows/how2mod/proxy/ClientProxy.java
+++ src/main/java/shadows/how2mod/proxy/ClientProxy.java
@@ -1,14 +1,21 @@
 package shadows.how2mod.proxy;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import shadows.how2mod.client.IHasModel;
+import shadows.how2mod.init.ModRegistry;
 
 public class ClientProxy extends CommonProxy {
 	
 	@Override
 	public void preInit(FMLPreInitializationEvent e) {
-
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@Override
@@ -19,6 +26,12 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void postInit(FMLPostInitializationEvent e) {
 
+	}
+	
+	@SubscribeEvent
+	public void onModelRegister(ModelRegistryEvent e) {
+		for(Block b : ModRegistry.BLOCKS) if(b instanceof IHasModel) ((IHasModel) b).registerModels();
+		for(Item i: ModRegistry.ITEMS) if(i instanceof IHasModel) ((IHasModel) i).registerModels();
 	}
 	
 }
