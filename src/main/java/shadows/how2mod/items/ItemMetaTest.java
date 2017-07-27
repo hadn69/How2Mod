50bb9d5cd670e883bccdba140dec05ddedd98469
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-27 05:00:39 -0400

    functional meta item

--- src/main/java/shadows/how2mod/items/ItemMetaTest.java
+++ src/main/java/shadows/how2mod/items/ItemMetaTest.java
@@ -1,9 +1,18 @@
 package shadows.how2mod.items;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 
 public class ItemMetaTest extends ItemTest {
@@ -34,6 +43,22 @@ public class ItemMetaTest extends ItemTest {
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		return super.getUnlocalizedName() + "." + stack.getMetadata();
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		ItemStack stack = player.getHeldItem(hand);
+		if (world.isRemote)
+			player.sendMessage(new TextComponentString("You have right clicked with a " + I18n.format(getUnlocalizedName(stack) + ".name")));
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		ItemStack stack = player.getHeldItem(hand);
+		if (world.isRemote)
+			player.sendMessage(new TextComponentString("You have right clicked a " + world.getBlockState(pos).getBlock().getRegistryName() + " with a " + I18n.format(getUnlocalizedName(stack) + ".name")));
+		return EnumActionResult.SUCCESS;
 	}
 
 }
