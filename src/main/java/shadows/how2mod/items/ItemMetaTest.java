1b048dd65316b61da021a2f607dce39eb7904ad8
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-28 02:46:07 -0400

    Creative Tab fake subtype item

--- src/main/java/shadows/how2mod/items/ItemMetaTest.java
+++ src/main/java/shadows/how2mod/items/ItemMetaTest.java
@@ -30,7 +30,7 @@ public class ItemMetaTest extends ItemTest {
 	@Override
 	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> list) {
 		if (this.isInCreativeTab(tab))
-			for (int i = 0; i < 16; i++)
+			for (int i = 0; i < 17; i++)
 				list.add(new ItemStack(this, 1, i));
 	}
 
