07f20379cadf37c167c52e980fc902e776c4779b
Shadows_of_Fire <Bward7864@gmail.com>
2017-07-27 21:13:15 -0400

    The block has states

--- src/main/java/shadows/how2mod/blocks/BlockWithStates.java
+++ src/main/java/shadows/how2mod/blocks/BlockWithStates.java
@@ -1,9 +1,31 @@
 package shadows.how2mod.blocks;
 
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+
 public class BlockWithStates extends BlockTest{
+	
+	public static final PropertyInteger META = PropertyInteger.create("meta", 0, 7);
 
 	public BlockWithStates(String name) {
 		super(name);
+		setDefaultState(this.blockState.getBaseState().withProperty(META, 0));
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(META);
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(META, meta);
+	}
+	
+	@Override
+	public BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, META);
 	}
 
 }
